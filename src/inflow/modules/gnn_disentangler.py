ecafad8a3c785558ccace190de17dd6e60dd669a
amirakbarnejad <aa36@sanger.ac.uk>
2024-09-09 15:35:55 +0100

    update

--- src/inflow/modules/gnn_disentangler.py
+++ src/inflow/modules/gnn_disentangler.py
@@ -221,15 +221,15 @@ class GNNDisentangler(nn.Module):
             torch.exp(
                 self.module_linearhead_sigmaxint(output_gnn_backbone)
             ),
-            min=0.001,  # TODO: maybe tune?
-            max=4.0
+            min=0.00001,  # TODO: maybe tune?
+            max=10.0
         )  # [N, num_genes]
         sigmaxspl_raw = torch.clamp(
             torch.exp(
                 self.module_linearhead_sigmaxspl(output_gnn_backbone)
             ),
-            min=0.001,  # TODO: maybe tune?
-            max=4.0
+            min=0.000001,  # TODO: maybe tune?
+            max=10.0
         )  # [N, num_genes]
 
         '''
