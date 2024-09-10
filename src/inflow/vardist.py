11d04ec793a4fc83fc0f8c561d35acef2d477701
amirakbarnejad <aa36@sanger.ac.uk>
2024-09-10 14:29:09 +0100

    update

--- src/inflow/vardist.py
+++ src/inflow/vardist.py
@@ -479,7 +479,7 @@ class InFlowVarDist(nn.Module):
                 "dim_u_spl": self.module_genmodel.dict_varname_to_dim['u_spl'],
                 "dim_CT":self.module_genmodel.dict_varname_to_dim['CT'],
                 "dim_NCC":self.module_genmodel.dict_varname_to_dim['NCC']
-            }  # how batch.y is split between u_int, u_spl
+            }  # how batch.y is split between u_int, u_spl, CT, and NCC
 
             ten_xy_touse = ten_xy_absolute + 0.0
             if prob_applytfm_affinexy > 0.0:
