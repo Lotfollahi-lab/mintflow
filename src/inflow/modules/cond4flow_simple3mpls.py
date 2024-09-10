f4b2a0d60ff88078bfaf74bbfb7b7f2c450cabe6
amirakbarnejad <aa36@sanger.ac.uk>
2024-09-10 18:39:40 +0100

    update

--- src/inflow/modules/cond4flow_simple3mpls.py
+++ src/inflow/modules/cond4flow_simple3mpls.py
@@ -7,22 +7,114 @@ import numpy as np
 import torch
 import torch.nn as nn
 from . import impanddisentgl
+from . import mlp
 
 
 class Cond4FlowVarphi0SimpleMLPs(nn.Module):
-    def __init__(self, dim_s:int, kwargs_genmodel, type_module_xbarint2z, kwargs_module_xbarint2z, type_module_xbarspl2sout, kwargs_module_xbarspl2sout, type_module_xbarspl2sin, kwargs_module_xbarspl2sin):
+    def __init__(self, kwargs_genmodel, encZ_list_dim_hidden, encSin_list_dim_hidden, encSout_list_dim_hidden, dict_varname_to_takeCT_takeNCC):
         '''
-        :param dim_s: dimension of s (i.e. s-in s and s-out s).
         :param kwargs_genmodel: so this module knows wheter to add cell-type/niche labels for z and s_out.
+        :param encZ_list_dim_hidden: list of hidden dimenions of the MLP encoder for Z (in dim and out dim are automatically determined).
+        :param dict_varname_to_takeCT_takeNCC: a dictionary with keys CT and NCC and values [take_CT:bool, take_NCC:bool]
         '''
         super(Cond4FlowVarphi0SimpleMLPs, self).__init__()
-        # self.sigma_sz = sigma_sz
         self.kwargs_genmodel = kwargs_genmodel
+        self.encZ_list_dim_hidden = encZ_list_dim_hidden
+        self.encSin_list_dim_hidden = encSin_list_dim_hidden
+        self.encSout_list_dim_hidden = encSout_list_dim_hidden
+        self.dict_varname_to_takeCT_takeNCC = dict_varname_to_takeCT_takeNCC
 
-        self.module_xbarint2z = type_module_xbarint2z(**kwargs_module_xbarint2z)  # when passed in, either operates on xbar_int or both [xbar_int, u_z]
-        self.module_xbarspl2sout = type_module_xbarspl2sout(**kwargs_module_xbarspl2sout)  # when passed in, either operates on xbar_int or both [xbar_int, u_z]
-        self.module_xbarspl2sin = type_module_xbarspl2sin(**kwargs_module_xbarspl2sin)  # when passed in, either operates on xbar_int or both [xbar_int, u_z]
+        self._check_args()
 
+        dim_s = self.kwargs_genmodel['dict_varname_to_dim']['s']
+
+        #create encoder modules
+        self.module_enc_z = mlp.SimpleMLP(
+            dim_input=dim_s + self._func_varname_to_dimextention('z'),
+            list_dim_hidden=encZ_list_dim_hidden,
+            dim_output=dim_s,
+            bias=True,
+            flag_endwithReLU=False,  # TODO:check
+            flag_startswithReLU=False  # TODO:check
+        )
+        self.module_enc_sin = mlp.SimpleMLP(
+            dim_input=dim_s + self._func_varname_to_dimextention('sin'),
+            list_dim_hidden=encSin_list_dim_hidden,
+            dim_output=dim_s,
+            bias=True,
+            flag_endwithReLU=False,  # TODO:check
+            flag_startswithReLU=False  # TODO:check
+        )
+        self.module_enc_sout = mlp.SimpleMLP(
+            dim_input=2*dim_s + self._func_varname_to_dimextention('sout'),
+            list_dim_hidden=encSout_list_dim_hidden,
+            dim_output=dim_s,
+            bias=True,
+            flag_endwithReLU=False,  # TODO:check
+            flag_startswithReLU=False  # TODO:check
+        )
+
+
+    @torch.no_grad()
+    def _func_extendby_CTNCC(self, x, batch, varname):
+        '''
+        Give a batch, returns the CT and NCC extension (if specified).
+        The output is to be concatenated to the encoder's input.
+        :param batch:
+        :return:
+        '''
+        assert (varname in ['z', 'sin', 'sout'])
+        output = [x]
+
+        if self.dict_varname_to_takeCT_takeNCC[varname][0]:
+            rng_CT = [
+                batch.INFLOWMETAINF['dim_u_int'] + batch.INFLOWMETAINF['dim_u_spl'],
+                batch.INFLOWMETAINF['dim_u_int'] + batch.INFLOWMETAINF['dim_u_spl'] + batch.INFLOWMETAINF['dim_CT']
+            ]
+            output.append(
+                batch.y[
+                    :,
+                    rng_CT[0]:rng_CT[1]
+                ]
+            )
+
+        if self.dict_varname_to_takeCT_takeNCC[varname][1]:
+            rng_NCC = batch.INFLOWMETAINF['dim_u_int'] + batch.INFLOWMETAINF['dim_u_spl'] + batch.INFLOWMETAINF['dim_CT']
+            output.append(
+                batch.y[
+                    :,
+                    rng_NCC:
+                ]
+            )
+
+        if len(output) > 1:
+            return torch.cat(output, 1)
+        else:
+            return output[0]
+
+
+    def _func_varname_to_dimextention(self, varname):
+        assert (varname in ['z', 'sin', 'sout'])
+        dim_toret = 0
+
+        if self.dict_varname_to_takeCT_takeNCC[varname][0]:
+            dim_toret += self.kwargs_genmodel['dict_varname_to_dim']['CT']
+
+        if self.dict_varname_to_takeCT_takeNCC[varname][1]:
+            dim_toret += self.kwargs_genmodel['dict_varname_to_dim']['NCC']
+
+        return dim_toret
+
+
+
+    def _check_args(self):
+        assert (
+            set(self.dict_varname_to_takeCT_takeNCC.keys()) == {'z', 'sin', 'sout'}
+        )
+        for varname in ['z', 'sin', 'sout']:
+            assert isinstance(self.dict_varname_to_takeCT_takeNCC[varname], list)
+            assert isinstance(self.dict_varname_to_takeCT_takeNCC[varname][0] in [True, False])
+            assert isinstance(self.dict_varname_to_takeCT_takeNCC[varname][1] in [True, False])
 
     def forward(self, ten_xbar_int, batch, ten_xbar_spl, ten_xy_absolute: torch.Tensor):
         '''
@@ -38,50 +130,36 @@ class Cond4FlowVarphi0SimpleMLPs(nn.Module):
         assert (
             batch.y.size()[1] == (batch.INFLOWMETAINF['dim_u_int'] + batch.INFLOWMETAINF['dim_u_spl'] + batch.INFLOWMETAINF['dim_CT'] + batch.INFLOWMETAINF['dim_NCC'])
         )
-        ten_uz = batch.y[:, 0:batch.INFLOWMETAINF['dim_u_int']].to(ten_xy_absolute.device) if (self.kwargs_genmodel['flag_use_int_u']) else None
-        ten_us = batch.y[
-             :,
-             batch.INFLOWMETAINF['dim_u_int']:batch.INFLOWMETAINF['dim_u_int']+batch.INFLOWMETAINF['dim_u_spl']
-        ].to(ten_xy_absolute.device) if (self.kwargs_genmodel['flag_use_spl_u']) else None
-
-        if ten_uz is None:
-            mu_z = self.module_xbarint2z(ten_xbar_int)  # [N, dim_z]
-        else:
-            mu_z = self.module_xbarint2z(
-                torch.cat([ten_xbar_int, ten_uz], 1)
-            )  # [N, dim_z]
 
 
-        if ten_us is None:
-            mu_sout = self.module_xbarspl2sout(
-                ten_xbar_spl
-            )  # [N, dim_s]
-        else:
-            mu_sout = self.module_xbarspl2sout(
-                torch.cat(
-                    [ten_xbar_spl, ten_us],
-                    1
-                )
-            )  # [N, dim_s]
-
-        if ten_us is None:
-            mu_sin = self.module_xbarspl2sin(
-                ten_xbar_spl
-            )  # [N, dim_s]
-        else:
-            mu_sin = self.module_xbarspl2sin(
-                torch.cat(
-                    [ten_xbar_spl, ten_us],
-                    1
-                )
-            )  # [N, dim_s]
+        mu_z = self.module_enc_z(
+            self._func_extendby_CTNCC(
+                x=ten_xbar_int,
+                batch=batch,
+                varname='z'
+            )
+        )
+        mu_sin = self.module_enc_sin(
+            self._func_extendby_CTNCC(
+                x=ten_xbar_spl,
+                batch=batch,
+                varname='sin'
+            )
+        )
+        mu_sout = self.module_enc_sout(
+            self._func_extendby_CTNCC(
+                x=torch.cat([ten_xbar_int, ten_xbar_spl], 1),
+                batch=batch,
+                varname='sout'
+            )
+        )
 
 
-
-        sigma_sz = torch.ones(
+        sigma_sz = 0.0001*torch.ones(
             size=[mu_z.size()[1]+mu_sin.size()[1]],
             device=mu_sin.device
         )  # TODO: change it to learnable but with lower-bound clipping.
+        # now `sigma_sz` is ignored (manually sigma values are used instead).
 
         return dict(
             mu_z=mu_z,
