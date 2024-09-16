67a4f0c02c97e554338cf077112855b68e4bd6f5
amirakbarnejad <aa36@sanger.ac.uk>
2024-09-16 10:09:37 +0100

    update

--- src/inflow/evaluation/predxspl.py
+++ src/inflow/evaluation/predxspl.py
@@ -33,10 +33,10 @@ class EvalXsplpred:
             try:
                 np_pred = np_pred - np.min(np_pred)
                 np_pred = np_pred / np_pred.max()
+                np_pred = np_pred * np_xobs[mask_nonzero_exp].flatten()
             except:
                 np_pred = np_xspl_pred[mask_nonzero_exp].flatten() + 0.0
 
-        np_pred = np_pred * np_xobs[mask_nonzero_exp].flatten()
         np_gt = np_xspl_gt[mask_nonzero_exp].flatten() + 0.0
 
         dict_toret = {}
@@ -45,4 +45,52 @@ class EvalXsplpred:
             dict_toret[measname] = measval
 
         return dict_toret
+
+
+
+class EvalLargeReadoutsXsplpred:
+    '''
+    Evaluates predXspl on large readouts (i.e. after excluding small readouts).
+    '''
+
+    def __init__(self, mincut_readout:int):
+        self.mincut_readout = mincut_readout
+        self.list_measures = [func_mse, func_mae]
+
+    def eval(self, np_xspl_gt:np.ndarray, np_xspl_pred:np.ndarray, np_xobs:np.ndarray, flag_normalize:bool):
+        assert (
+            isinstance(np_xspl_gt, np.ndarray)
+        )
+        assert (
+            isinstance(np_xspl_pred, np.ndarray)
+        )
+        assert (
+            isinstance(np_xobs, np.ndarray)
+        )
+
+        set_cnts = list(
+            set(np_xobs[np_xobs >= self.mincut_readout].flatten().tolist())
+        )
+        set_cnts.sort()
+
+        for min_count in set_cnts:
+            mask_min_exp = (np_xobs >= min_count)
+            np_pred = np_xspl_pred[mask_min_exp].flatten() + 0.0
+            if flag_normalize:
+                try:
+                    np_pred = np_pred - np.min(np_pred)
+                    np_pred = np_pred / np_pred.max()
+                    np_pred = np_pred * np_xobs[mask_min_exp].flatten()
+                except:
+                    np_pred = np_xspl_pred[mask_min_exp].flatten() + 0.0
+
+            np_gt = np_xspl_gt[mask_min_exp].flatten() + 0.0
+
+            dict_toret = {}
+            for measure in self.list_measures:
+                measname, measval = measure(np_pred, np_gt)
+                dict_toret["{} (among readout >= {})".format(measname, min_count)] = measval
+
+
+
 
