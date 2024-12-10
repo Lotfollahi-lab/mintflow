b4757f2af93079f21fd49ad5d85f5b66cfe60ad8
amirakbarnejad <aa36@sanger.ac.uk>
2024-12-10 15:13:32 +0000

    Revert "update"
    This reverts commit 1da5c8fb1cdb34aab7f88b4f32c1c50ea350be60.

--- src/inflow/utils_multislice.py
+++ src/inflow/utils_multislice.py
@@ -144,10 +144,10 @@ class Slice:
                 indices=self.adata.X.tocoo().nonzero(),
                 values=self.adata.X.tocoo().data,
                 size=self.adata.X.tocoo().shape
-            ).float().detach(),
-            edge_index=self.edge_index.detach(),
+            ).float(),
+            edge_index=self.edge_index,
             y=torch.cat(
-                [ten_u_z.detach(), ten_u_s.detach(), self.ten_CT.detach(), self.ten_NCC.detach()],
+                [ten_u_z, ten_u_s, self.ten_CT, self.ten_NCC],
                 1
             )
         )
