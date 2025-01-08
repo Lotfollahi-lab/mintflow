8474bb7c79e49ecea3e6b3d86e4f71d60d18235d
amirakbarnejad <aa36@sanger.ac.uk>
2025-01-08 10:13:34 +0000

    update

--- src/inflow/cli/check_listtissue_trtest.py
+++ src/inflow/cli/check_listtissue_trtest.py
@@ -7,6 +7,7 @@ Extensive checks on training and testing list tissues.
 import os, sys
 import numpy as np
 from .. import utils_multislice
+from pprint import pprint
 
 def check(train_list_slice:utils_multislice.ListSlice, test_list_slice:utils_multislice.ListSlice):
     assert isinstance(train_list_slice, utils_multislice.ListSlice)
@@ -42,7 +43,42 @@ def check(train_list_slice:utils_multislice.ListSlice, test_list_slice:utils_mul
                 )
 
 
-    # TODO:HERE check if the testing batch IDs are a subset of training batch IDs
+    # check if the testing batch IDs are a subset of training batch IDs
+    set_bid_train = set([
+        sl._get_batchid() for sl in train_list_slice
+    ])
+    set_bid_test = set([
+        sl._get_batchid() for sl in test_list_slice
+    ])
 
-    # TODO: check if the mapping dicts in two list slice-s are the same.
+    if not set_bid_test.issubset(set_bid_train):
+        raise Exception(
+            "The following biobatch identifiers are present in the test set, while they haven't been present in the training set. This is prohibited. \n {}".format(
+                set_bid_test.difference(set_bid_train)
+            )
+        )
 
+
+
+    # check if the mapping dicts in two list slice-s are the same.
+    if train_list_slice.map_CT_to_inflowCT != test_list_slice.map_CT_to_inflowCT:
+        print("train_list_slice.map_CT_to_inflowCT:")
+        pprint(train_list_slice.map_CT_to_inflowCT)
+
+        print("test_list_slice.map_CT_to_inflowCT:")
+        pprint(test_list_slice.map_CT_to_inflowCT)
+
+        raise Exception(
+            "Training list tissue and testing list tissue do not have the same `map_CT_to_inflowCT`. See above for the content of each."
+        )
+
+    if train_list_slice.map_Batchname_to_inflowBatchID != test_list_slice.map_Batchname_to_inflowBatchID:
+        print("train_list_slice.map_Batchname_to_inflowBatchID:")
+        pprint(train_list_slice.map_Batchname_to_inflowBatchID)
+
+        print("test_list_slice.map_Batchname_to_inflowBatchID:")
+        pprint(test_list_slice.map_Batchname_to_inflowBatchID)
+
+        raise Exception(
+            "Training list tissue and testing list tissue do not have the same `map_Batchname_to_inflowBatchID`. See above for the content of each."
+        )
