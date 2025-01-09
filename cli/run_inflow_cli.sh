865f83a277af599dee07610d8be99e463bd9b644
amirakbarnejad <aa36@sanger.ac.uk>
2025-01-09 09:33:06 +0000

    update

---
+++ cli/run_inflow_cli.sh
@@ -0,0 +1,11 @@
+
+
+python inflow_cli.py \
+--file_config_data_train config_data_train.yml \
+--file_config_data_test config_data_test.yml \
+--file_config_model config_model.yml \
+--file_config_training config_training.yml \
+--path_output "./NonGit/Output_Path" \
+--flag_verbose "True" \
+
+
