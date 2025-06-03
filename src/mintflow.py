ba6c1c593c7cd62d1fc13590e2f5787f58d81fc7
Sebastian Birk <sebastian.birk@hpc-build01.scidom.de>
2025-06-03 16:41:40 +0200

    Remove mintflow synth

---
+++ src/mintflow.py
@@ -0,0 +1,15 @@
+import anndata as ad
+
+
+class MintFlow:
+    def __init__(self,
+                 config: dict,
+                 adata: ad.AnnData):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
