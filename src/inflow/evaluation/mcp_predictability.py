d91c1aeb9816ebb41467b524b9196d04a5614b6d
amirakbarnejad <aa36@sanger.ac.uk>
2025-06-04 12:12:22 +0100

    update

--- src/inflow/evaluation/mcp_predictability.py
+++ src/inflow/evaluation/mcp_predictability.py
@@ -121,10 +121,10 @@ class ListGeneMicScore:
         )  # [N x num_selgenes] and dense
 
         # create the dataframe toreturn
-        print(X_before_scppnormalizetotal[mask_readcount].shape)
-        print(fraction_Xmic[mask_readcount].shape)
-        print(np_r2score_amongfoundgenes[mask_readcount].shape)
-        
+        print(X_before_scppnormalizetotal.shape)
+        print(fraction_Xmic.shape)
+        print(np_r2score_amongfoundgenes.shape)
+
         df_toret = pd.DataFrame(
             np.stack([
                 X_before_scppnormalizetotal[mask_readcount],
