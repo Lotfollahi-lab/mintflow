74b74f0bb174461c791105389be71babce737d11
amirakbarnejad <aa36@sanger.ac.uk>
2025-06-07 20:18:02 +0100

    update

--- src/mintflow/interface/__init__.py
+++ src/mintflow/interface/__init__.py
@@ -1,3 +1,5 @@
 
 
 from . import parse_config_data_train, module_parse_config_data_evaluation, parse_config_training, parse_config_model
+
+from .module_parse_config_data_evaluation import parse_config_data_evaluation
