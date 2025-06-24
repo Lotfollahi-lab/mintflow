5d59b89260d91e7a6d175efaf3e48cf947469f25
amirakbarnejad <aa36@sanger.ac.uk>
2025-06-24 21:35:34 +0100

    update

---
+++ src/mintflow/interface/perturbation/module_gen_stdata.py
@@ -0,0 +1,146 @@
+
+
+
+import random
+from typing import Dict, List
+
+import anndata
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+import gc
+import torch
+import torch_geometric as pyg
+from torch_geometric.utils.convert import from_scipy_sparse_matrix
+
+from . import module_gen_micsizefactor
+
+from .. import base_interface
+from ...evaluation import base_evaluation
+
+from ... import vardist, utils_multislice
+from .. import module_predict
+
+from ...modules.gnn import KhopAvgPoolWithoutselfloop
+
+
+@torch.no_grad()
+def generate_ST_data(
+    adata:anndata.AnnData,
+    obskey_celltype:str,
+    obspkey_neighbourhood_graph:str,
+    device,
+    batch_index_trainingdata:int,
+    model:vardist.InFlowVarDist,
+    data_mintflow:Dict,
+    dict_all4_configs:Dict,
+    evalulate_on_sections: List[int] | List[str] | str,
+    kwargs_Kmeans_MCC=None,
+    kwargs_pygdl_computeMCC=None,
+):
+    """
+
+    :param adata:
+    :param obskey_celltype:
+    :param batch_index_trainingdata:
+    :param obspkey_neighbourhood_graph:
+    :param device
+    :param model:
+    :param data_mintflow:
+    :param dict_all4_configs:
+    :param evalulate_on_sections:
+    :param kwargs_Kmeans_MCC:
+    :param kwargs_pygdl_computeMCC
+    :return:
+    """
+
+    if kwargs_Kmeans_MCC is None:
+        kwargs_Kmeans_MCC = {'n_clusters': 10, 'random_state': 0, 'n_init': "auto"}
+
+    if kwargs_pygdl_computeMCC is None:
+        kwargs_pygdl_computeMCC = {
+            'batch_size': 10,
+            'num_workers': 0,
+            'num_neighbors': [-1]
+        }
+
+
+
+    # check args
+    base_interface.check_arg_data_mintflow(data_mintflow=data_mintflow)
+    base_interface.checkif_4configs_are_verified(dict_all4_configs=dict_all4_configs)
+
+    obj_sizefacgenerator = module_gen_micsizefactor.GeneratorMicSizeFactor(
+        model=model,
+        data_mintflow=data_mintflow,
+        dict_all4_configs=dict_all4_configs,
+        evalulate_on_sections=evalulate_on_sections,
+        kwargs_Kmeans_MCC=kwargs_Kmeans_MCC
+    )
+
+    # check that there are no novel cell types in the CT col
+    if obskey_celltype not in adata.obs.columns:
+        raise Exception(
+            "The provided `obskey_celltype = {}` is not among the columns of the `.obs` field of the provided anndata object.".format(
+                obskey_celltype
+            )
+        )
+
+    if set(adata.obs[obskey_celltype]).difference(set(data_mintflow['train_list_tissue_section'].map_CT_to_inflowCT.keys())) != set([]):
+        raise Exception(
+            "The `{}` column of the `.obs` field of the provided anndata object contains the following cell types which weren't present in training set: {}".format(
+                obskey_celltype,
+                set(adata.obs[obskey_celltype]).difference(
+                    set(data_mintflow['train_list_tissue_section'].map_CT_to_inflowCT.keys())
+                )
+            )
+        )
+    list_CTindex = [
+        int(
+            data_mintflow['train_list_tissue_section'].map_CT_to_inflowCT[
+                adata.obs.iloc[n][obskey_celltype]
+            ].split("_")[1]
+        )
+        for n in range(adata.shape[0])
+    ]
+    ten_CT = torch.eye(len(set(data_mintflow['train_list_tissue_section'].map_CT_to_inflowCT.keys())))[
+        list_CTindex,
+        :
+    ]
+
+
+    # compute edge_index from the neighbourhood graph
+    edge_index, _ = from_scipy_sparse_matrix(
+        adata.obsp[obspkey_neighbourhood_graph]
+    )  # [2, num_edges]
+    edge_index = torch.Tensor(
+        pyg.utils.remove_self_loops(pyg.utils.to_undirected(edge_index))[0]
+    )
+
+    # compute np_MCC (needed for generating micenv size factors)
+    module_compNCC = KhopAvgPoolWithoutselfloop(
+        num_hops=dict_all4_configs['config_model']['num_graph_hops'],
+        dim_input=None,
+        dim_output=None
+    )
+    module_compNCC = module_compNCC.to(device)
+    ten_MCC = module_compNCC.evaluate_layered(
+        x=ten_CT.to(device),
+        edge_index=edge_index.to(device),
+        kwargs_dl=kwargs_pygdl_computeMCC
+    )
+
+
+
+    # generate mic size factors
+    list_idx_MCCcluster = obj_sizefacgenerator.kmeans.predict(ten_MCC.detach().cpu().numpy())
+    print(list_idx_MCCcluster)
+    print(type(list_idx_MCCcluster))
+
+    obj_sizefacgenerator.gen_sizefactors(
+        list_idx_CT=list_CTindex,
+        list_idx_MCCcluster=list_idx_MCCcluster
+    )
+
+
+
