6502e050444a941bd91d3e0eeeb0b332dc31cabf
Sebastian Birk <sebastian.birk@outlook.com>
2025-07-04 10:42:21 +0200

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,10 @@
 import pytest
 
-import celldino
+import mintflow
 
 
 def test_package_has_version():
-    assert celldino.__version__ is not None
+    assert mintflow.__version__ is not None
 
 
 @pytest.mark.skip(reason="This decorator should be removed when test passes.")
