255eef77cd4477c02dad9e2218623a1eff34b584
Sebastian Birk <sebastian.birk@outlook.com>
2025-07-04 11:03:40 +0200

    Update __init__.py

--- src/mintflow/__init__.py
+++ src/mintflow/__init__.py
@@ -1,49 +1,7 @@
-#
-# # general imports ====
-#
-# # mintflow imports ====
-#
-#
-# from . import utils
-#
-# from . import generativemodel # exec('import {}.generativemodel'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from . import modules  # exec('import {}.modules.gnn'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from .modules import gnn, neuralODE, mlp, disentonly
-#
-#
-# from .modules.impanddisentgl import  MaskLabel #import exec('from {}.modules.impanddisentgl import MaskLabel'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from . import vardist  # exec('import {}.vardist'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from . import masking # exec('import {}.masking'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from .modules.impanddisentgl import ImputerAndDisentangler # exec('from {}.modules.impanddisentgl import ImputerAndDisentangler'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from .modules.disentonly import Disentangler # exec('from {}.modules.disentonly import Disentangler'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from .modules.disentonly_twosep import DisentanglerTwoSep # exec('from {}.modules.disentonly_twosep import DisentanglerTwoSep'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from .zs_samplers import RandomZSSampler, PerCelltypeZSSampler #exec('from {}.zs_samplers import RandomZSSampler, PerCelltypeZSSampler'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from .predadjmat import ListAdjMatPredLoss, AdjMatPredLoss #  exec('from {}.predadjmat import ListAdjMatPredLoss, AdjMatPredLoss'.format(STR_INFLOW_OR_INFLOW_SYNTH))
-# from .utils_flowmatching import ModeSampleX0, ModeMinibatchPerm, ModeTimeSched, ModeFMLoss, ConditionalFlowMatcher
-#
-# from .modules.cond4flow import Cond4FlowVarphi0
-#
-# from .modules.cond4flow_simple3mpls import Cond4FlowVarphi0SimpleMLPs
-#
-# from .utils_pyg import PygSTDataGridBatchSampler
-#
-#
-# from .evaluation.bioconsv import EvaluatorKmeans, EvaluatorLeiden
-#
-# from .evaluation.predxspl import EvalXsplpred, EvalLargeReadoutsXsplpred, EvalOnHVGsXsplpred
-#
-# from .modules.gnn_disentangler import GNNDisentangler
-#
-# from .kl_annealing import LinearAnnealingSchedule
-#
-# from .modules.predictorperCT import PredictorPerCT
-#
-# from .utils_multislice import ListSlice, Slice
-#
-# from .modules.varphienc4xbar import EncX2Xbar
-#
-# from .modules.predictorbatchID import PredictorBatchID
-#
+from importlib.metadata import version
+
+__all__ = ["data", "evaluation", "interface", "lrdb", "modules"]
+__version__ = version("mintflow")
 
 from .interface import \
     verify_and_postprocess_config_data_train,\
